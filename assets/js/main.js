682aaeb7bc65fa0480f7fe494805494177f00969
oussamarejab <brodservicess@gmail.com>
2021-12-26 13:32:29 +0300

    olkji
    asxe

--- assets/js/main.js
+++ assets/js/main.js
@@ -233,3 +233,18 @@
     });
 
 })()
+
+
+//<!-- track visitor heated map -->
+
+window._mfq = window._mfq || [];
+(function() {
+    var mf = document.createElement("script");
+    mf.type = "text/javascript";
+    mf.defer = true;
+    mf.src = "//cdn.mouseflow.com/projects/02f02da4-e459-4261-838c-4af98989d833.js";
+    document.getElementsByTagName("head")[0].appendChild(mf);
+})();
+
+
+//<!-- /////////track visitor heated map -->
